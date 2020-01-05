60cd21a9879faaf0fe33c84bf77be124a29f3d51
luliang666666 <59439249+luliang666666@users.noreply.github.com>
2020-01-05 14:37:14 +0800

    Update AnaphaseApplication.java

--- src/main/java/com/baizhi/AnaphaseApplication.java
+++ src/main/java/com/baizhi/AnaphaseApplication.java
@@ -10,5 +10,6 @@ public class AnaphaseApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AnaphaseApplication.class, args);
+           System.out.println("88888888");
     }
 }
